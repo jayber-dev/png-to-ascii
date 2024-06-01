ab95b4fc0c4ac5b53c5a3a0003beb03609d5272a
jayber <jayber1@gmail.com>
2024-06-01 19:56:56 +0300

    +

--- engine.js
+++ engine.js
@@ -11,8 +11,13 @@ const hexMap = {
   e: 14,
   f: 15,
 }
-function readChunkType() {
-
+function readChunkType(stream, offset, chunkLength) {
+  let typeString = ""
+  for (let i = offset + 4; i < offset + 8; i++) {
+    typeString += String.fromCharCode(stream[i])
+    //    console.log(String.fromCharCode(stream[i]))
+  }
+  return typeString
 }
 
 function concatData(stream) {
@@ -24,14 +29,24 @@ function calcNextOffset(stream, offset, chunkLength, chunkLayout) {
   console.log("chunkLayout:", chunkLayout)
   let dataLength = 0
   for (let i = offset; i <= offset + chunkLength - 1; i++) {
-    console.log(`byte: ${i}`, stream[i].toString(16).padStart(2, '0'))
-
+    //    console.log(`byte: ${i}`, stream[i].toString(16).padStart(2, '0'))
     dataLength = dataLength << 8 | stream[i]
   }
 
-  console.log(dataLength)
+  console.log("length of chunk Data: ", dataLength)
+  const chunkType = readChunkType(stream, offset, chunkLength)
+  console.log(chunkType)
   // readData(stream)
-  return dataLength + chunkLayout + offset
+
+  if (chunkType === 'IDAT') {
+    console.log('------------------------------------------------------')
+    return dataLength + chunkLayout + offset
+  } else if (chunkType !== 'IEND') {
+    console.log('------------------------------------------------------')
+    return dataLength + chunkLayout + offset
+  } else {
+    return -1
+  }
 }
 
 
@@ -40,11 +55,9 @@ async function main() {
     const stream = await fs.readFile('./frame.png')
     //console.log(stream[33].toString(16).padStart(2, '0'))
     let chunkLength = 4
-    let chunkType = 4
     let currentOffset = 8
     let dataLength = 12
     let decimal = 0
-    let nextOffset = 0
     let chunkLayout = 12
 
     // calculate the lenght of the IHDR
@@ -55,25 +68,19 @@ async function main() {
     //    dataLength += Number(stream[i].toString(10))
 
     //}
-    console.log(decimal)
     currentOffset = calcNextOffset(stream, currentOffset, chunkLength, chunkLayout)
     console.log("next current offset: ", currentOffset)
-    const doOffset = (currentOffset)
     currentOffset = calcNextOffset(stream, currentOffset, chunkLength, chunkLayout)
-    dataLength = 12
-    console.log('------------------------------------------------------')
     // calculate the length of IDAT
-    for (let i = doOffset; i < doOffset + chunkLength; i++) {
-      //    console.log(`byte: ${i}`, stream[i].toString(10).padStart(2, '0'))
-      console.log(`byte: ${i}`, stream[i].toString(16).padStart(2, '0'))
-      decimal = decimal << 8 | stream[i]
-      dataLength += Number(stream[i].toString(10))
-    }
-    console.log(decimal)
-
-
-
-
+    //    for (let i = doOffset; i < doOffset + chunkLength; i++) {
+    //     //    console.log(`byte: ${i}`, stream[i].toString(10).padStart(2, '0'))
+    //   console.log(`byte: ${i}`, stream[i].toString(16).padStart(2, '0'))
+    //  decimal = decimal << 8 | stream[i]
+    //  dataLength += Number(stream[i].toString(10))
+    // }
+    //    console.log(decimal)
+    currentOffset = calcNextOffset(stream, currentOffset, chunkLength, chunkLayout)
+    console.log(currentOffset)
   } catch (error) {
     console.log(error)
   }
